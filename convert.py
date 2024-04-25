636f98e85b021671a3b1b43c8f911734bc5ae27c
shuu-wasseo <joshuasoh2008@gmail.com>
2024-04-25 10:31:44 +0800

    augmentations

--- convert.py
+++ convert.py
@@ -23,6 +23,75 @@ def find_latest_csv_downloads():
     latest_csv_file = max(csv_files, key=os.path.getmtime)
     return str(latest_csv_file)
 
+# Define augmentation functions for each category
+geometric_transforms = {
+    "rotation": ia.augmenters.Affine(rotate=(-45, 45)),
+    "scaling": ia.augmenters.Affine(scale={"x": (0.5, 1.5), "y": (0.5, 1.5)}),
+    "translation": ia.augmenters.Affine(translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)}),
+    "shearing": ia.augmenters.Affine(shear=(-20, 20)),
+    "perspective_transformations": ia.augmenters.PerspectiveTransform(scale=(0.01, 0.15)),
+    "cropping_and_padding": ia.augmenters.CropAndPad(percent=(-0.1, 0.1))
+}
+
+flipping = {
+    "horizontal_flip": ia.augmenters.Fliplr(1.0),
+    "vertical_flip": ia.augmenters.Flipud(1.0)
+}
+
+color_space_manipulation = {
+    "brightness_adjustment": ia.augmenters.MultiplyBrightness((0.5, 1.5)),
+    "contrast_adjustment": ia.augmenters.GammaContrast((0.5, 2.0)),
+    "saturation_adjustment": ia.augmenters.MultiplySaturation((0.5, 1.5)),
+    "hue_adjustment": ia.augmenters.MultiplyHue((0.5, 1.5)),
+    "color_channel_shifting": ia.augmenters.ChannelShuffle(p=1.0),
+    "gamma_correction": ia.augmenters.GammaContrast((0.5, 2.0))
+}
+
+noise_injection = {
+    "gaussian_noise": ia.augmenters.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.1*255)),
+    "salt_and_pepper_noise": ia.augmenters.SaltAndPepper(0.05),
+    "speckle_noise": ia.augmenters.Salt(0.05)
+    # Add more noise injections as needed
+}
+
+blur_and_sharpen = {
+    "gaussian_blur": ia.augmenters.GaussianBlur(sigma=(0.0, 3.0)),
+    "average_blur": ia.augmenters.AverageBlur(k=(2, 7)),
+    "median_blur": ia.augmenters.MedianBlur(k=(3, 11)),
+    "bilateral_blur": ia.augmenters.BilateralBlur(d=(3, 10)),
+    "sharpening": ia.augmenters.Sharpen(alpha=(0.0, 1.0), lightness=(0.75, 2.0))
+}
+
+distortion = {
+    "elastic_deformations": ia.augmenters.ElasticTransformation(alpha=(0.5, 3.5), sigma=0.25),
+    "piecewise_affine_transformations": ia.augmenters.PiecewiseAffine(scale=(0.01, 0.05)),
+    "perspective_warping": ia.augmenters.PerspectiveTransform(scale=(0.01, 0.15))
+}
+
+augmenters_combining_multiple_techniques = {
+    "sequential_augmentation": ia.augmenters.Sequential([
+        ia.augmenters.Affine(rotate=(-45, 45)),
+        ia.augmenters.Fliplr(0.5),
+        ia.augmenters.MultiplyBrightness((0.5, 1.5))
+    ]),
+    # "someof_augmentation": ia.augmenters.SomeOf(2, [
+    #     ia.augmenters.Affine(rotate=(-45, 45)),
+    #     ia.augmenters.Fliplr(0.5),
+    #     ia.augmenters.MultiplyBrightness((0.5, 1.5))
+    # ]),
+    # "oneof_augmentation": ia.augmenters.OneOf([
+    #     ia.augmenters.Affine(rotate=(-45, 45)),
+    #     ia.augmenters.Fliplr(0.5),
+    #     ia.augmenters.MultiplyBrightness((0.5, 1.5))
+    # ])
+}
+
+all_augmentations = {**geometric_transforms, **flipping, **color_space_manipulation,
+                     **noise_injection, **blur_and_sharpen, **distortion,
+                     **augmenters_combining_multiple_techniques}
+
+augmentations_list = all_augmentations.keys()
+
 def csv_to_yolo(yolo_dir):
     """
     Convert a CSV file containing bounding box annotations to YOLO format and apply augmentations.
@@ -73,17 +142,30 @@ def csv_to_yolo(yolo_dir):
             except json.JSONDecodeError:
                 print(f"Warning: Unable to parse JSON in row '{row}'")
                 continue
+        print("csv file processed")
+
+    # Clear out existing augmented images
+    for filename in os.listdir(images_dir):
+        if "-aug" in filename:
+            os.remove(os.path.join(images_dir, filename))
+
+    # Clear out existing augmented YOLO files
+    for filename in os.listdir(labels_dir):
+        if "-aug" in filename:
+            os.remove(os.path.join(labels_dir, filename))
 
     # Iterate through images and augmentations
     for filename, bounding_boxes in image_bbox_dict.items():
+        print("starting", filename, bounding_boxes)
         # Load the image
         image_path = os.path.join(images_dir, filename)
         image = Image.open(image_path)
 
         # Apply augmentations
-        augmentation_types = ["rotation", "scaling"]  # Add more augmentations here...
+        augmentation_types = all_augmentations  # Add more augmentations here...
         for idx, aug_type in enumerate(augmentation_types):
-            augmented_image, augmented_bounding_boxes = apply_augmentation(image, bounding_boxes, aug_type)
+            print("doing", aug_type)
+            augmented_image = apply_augmentation(image, bounding_boxes, aug_type)
 
             # Save augmented image
             aug_image_filename = f"{os.path.splitext(filename)[0]}-aug{idx}.jpg"
@@ -93,7 +175,7 @@ def csv_to_yolo(yolo_dir):
             # Save corresponding text file with YOLO format
             yolo_filename = os.path.join(labels_dir, os.path.splitext(filename)[0] + f"-aug{idx}.txt")
             with open(yolo_filename, 'w') as yolo_file:
-                for box in augmented_bounding_boxes:
+                for box in bounding_boxes:
                     x_center = (box[0] + box[2]) / (2.0 * image.width)
                     y_center = (box[1] + box[3]) / (2.0 * image.height)
                     box_width = (box[2] - box[0]) / image.width
@@ -103,132 +185,44 @@ def csv_to_yolo(yolo_dir):
 
     print("\n\nConversion and augmentation completed.\n\n")
 
+
 def apply_augmentation(image, bounding_boxes, augmentation_name):
     """
     Apply the specified augmentation to the image and bounding boxes.
 
     Args:
-        image (numpy.ndarray): The input image.
+        image (PIL.Image): The input image.
         bounding_boxes (list of tuples): List of bounding boxes in the format [(x_min, y_min, x_max, y_max), ...].
         augmentation_name (str): Name of the augmentation to apply.
 
     Returns:
         numpy.ndarray: The augmented image.
         list of tuples: List of augmented bounding boxes.
-    """
-
+    """ 
+    # Convert PIL image to numpy array
     image_np = np.array(image)
-
-    # Define augmentation functions for each category
-    geometric_transforms = {
-        "rotation": iaa.Affine(rotate=(-45, 45)),
-        "scaling": iaa.Affine(scale={"x": (0.5, 1.5), "y": (0.5, 1.5)}),
-        "translation": iaa.Affine(translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)}),
-        "shearing": iaa.Affine(shear=(-20, 20)),
-        "perspective_transformations": iaa.PerspectiveTransform(scale=(0.01, 0.15)),
-        "cropping_and_padding": iaa.CropAndPad(percent=(-0.1, 0.1))
-    }
-
-    flipping = {
-        "horizontal_flip": iaa.Fliplr(1.0),
-        "vertical_flip": iaa.Flipud(1.0)
-    }
-
-    color_space_manipulation = {
-        "brightness_adjustment": iaa.MultiplyBrightness((0.5, 1.5)),
-        "contrast_adjustment": iaa.GammaContrast((0.5, 2.0)),
-        "saturation_adjustment": iaa.MultiplySaturation((0.5, 1.5)),
-        "hue_adjustment": iaa.MultiplyHue((0.5, 1.5)),
-        "color_channel_shifting": iaa.ChannelShuffle(p=1.0),
-        "gamma_correction": iaa.GammaContrast((0.5, 2.0))
-    }
-
-    noise_injection = {
-        "gaussian_noise": iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.1*255)),
-        "salt_and_pepper_noise": iaa.SaltAndPepper(0.05),
-        "speckle_noise": iaa.Salt(0.05)
-        # Add more noise injections as needed
-    }
-
-    blur_and_sharpen = {
-        "gaussian_blur": iaa.GaussianBlur(sigma=(0.0, 3.0)),
-        "average_blur": iaa.AverageBlur(k=(2, 7)),
-        "median_blur": iaa.MedianBlur(k=(3, 11)),
-        "bilateral_blur": iaa.BilateralBlur(d=(3, 10)),
-        "sharpening": iaa.Sharpen(alpha=(0.0, 1.0), lightness=(0.75, 2.0))
-    }
-
-    distortion = {
-        "elastic_deformations": iaa.ElasticTransformation(alpha=(0.5, 3.5), sigma=0.25),
-        "piecewise_affine_transformations": iaa.PiecewiseAffine(scale=(0.01, 0.05)),
-        "perspective_warping": iaa.PerspectiveTransform(scale=(0.01, 0.15))
-    }
-
-    augmenters_combining_multiple_techniques = {
-        "sequential_augmentation": iaa.Sequential([
-            iaa.Affine(rotate=(-45, 45)),
-            iaa.Fliplr(0.5),
-            iaa.MultiplyBrightness((0.5, 1.5))
-        ]),
-        "someof_augmentation": iaa.SomeOf(2, [
-            iaa.Affine(rotate=(-45, 45)),
-            iaa.Fliplr(0.5),
-            iaa.MultiplyBrightness((0.5, 1.5))
-        ]),
-        "oneof_augmentation": iaa.OneOf([
-            iaa.Affine(rotate=(-45, 45)),
-            iaa.Fliplr(0.5),
-            iaa.MultiplyBrightness((0.5, 1.5))
-        ])
-    }
-
-    # Combine all dictionaries into one
-    all_augmentations = {
-        **geometric_transforms, 
-        **flipping, 
-        **color_space_manipulation,
-        **noise_injection, 
-        **blur_and_sharpen, 
-        **distortion,
-        **augmenters_combining_multiple_techniques, 
-    }
 
     # Check if the provided augmentation name is valid
     if augmentation_name not in all_augmentations:
         print("Invalid augmentation name. Available augmentations:")
         print(list(all_augmentations.keys()))
-        return None, None
+        return None 
 
     # Apply the selected augmentation to the image
     aug_function = all_augmentations[augmentation_name]
-    augmented_image = aug_function(image=image_np)
+    augmented_image_np = aug_function(image=image_np)
 
-    augmented_image_pil = Image.fromarray(augmented_image)
+    # Convert augmented image back to PIL format
+    augmented_image_pil = Image.fromarray(augmented_image_np)
 
     # Convert bounding boxes to imgaug format (BoundingBoxesOnImage)
-    bb_list = []
-    for box in bounding_boxes:
-        bb_list.append(ia.BoundingBox(x1=box[0], y1=box[1], x2=box[2], y2=box[3]))
-    bbs = ia.BoundingBoxesOnImage(bb_list, shape=image.shape)
+    bb_list = [ia.BoundingBox(x1=bb[0], y1=bb[1], x2=bb[2], y2=bb[3]) for bb in bounding_boxes]
+    bbs = ia.BoundingBoxesOnImage(bb_list, shape=image_np.shape)
 
     # Apply the augmentation to the bounding boxes
     bbs_aug = aug_function(bounding_boxes=bbs)
 
-    # Convert the augmented bounding boxes back to the original format
-    augmented_bounding_boxes = []
-    for bb in bbs_aug.bounding_boxes:
-        augmented_bounding_boxes.append((bb.x1, bb.y1, bb.x2, bb.y2))
-
-    return augmented_image_pil, augmented_bounding_boxes
-
-# Print a list of all available augmentations
-def all_augmentations():
-    return (
-        list(iaa.geometric_transforms.keys()) + list(iaa.flipping.keys()) + list(iaa.color_space_manipulation.keys()) + \
-        list(iaa.noise_injection.keys()) + list(iaa.blur_and_sharpen.keys()) + list(iaa.distortion.keys()) + \
-        list(iaa.augmenters_combining_multiple_techniques.keys()) + list(iaa.custom_augmenters.keys())
-    )
-
+    return augmented_image_pil 
 
 if __name__ == "__main__":
     # Check if the correct number of arguments are provided
