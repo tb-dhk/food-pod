8f81a6cf10d35bb533d70329d6036b1582058bae
shuu-wasseo <joshuasoh2008@gmail.com>
2024-04-25 14:17:10 +0800

    augmentations

--- main.py
+++ main.py
@@ -24,24 +24,7 @@ prev_loss = float('inf')  # Initialize with a very large value
 threshold_factor = 1.5  # 50% increase threshold
 
 # Training
-for epoch in range(epochs):
-    print("epoch", epoch + 1)
-    # Train the model for one epoch
-    results = model.train(data=data_config, epochs=1, imgsz=640, batch=batch_size, lr0=learning_rate, device=device)
-
-    # Get the training loss
-    train_loss = results[0]['total_loss']
-
-    # Check if the loss increased by 50%
-    if train_loss > prev_loss * threshold_factor:
-        print(f"Training stopped at epoch {epoch + 1} due to loss increase of over 50%.")
-        break
-
-    # Update previous loss for next iteration
-    prev_loss = train_loss
-
-    # Print training progress
-    print(f"Epoch [{epoch + 1}/{epochs}], Loss: {train_loss:.4f}")
+results = model.train(data=data_config, epochs=100, imgsz=640, batch=batch_size, lr0=learning_rate, device=device)
 
 print("Training Completed!")
 
