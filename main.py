9e3b8dbff8a270988b4983c7f30b5faf4f319cab
shuu-wasseo <joshuasoh2008@gmail.com>
2024-05-09 10:12:20 +0800

    .

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 epochs = 100
 batch_size = 8
 learning_rate = 0.001
-validation_interval = 1  # Evaluate on validation set after each epoch
+validation_interval = 5  # Evaluate on validation set after each epoch
 
 # Model loading
 model = YOLO("yolov8s.pt").to(device)  # Replace 'yolov8s.pt' with your pre-trained weights file
@@ -36,6 +36,7 @@ prev_loss = float('inf')  # Initialize with a very large value
 threshold_factor = 1.5  # 50% increase threshold
 
 for epoch in range(epochs):
+    print("epoch", epoch)
     # Training
     train_results = model.train(data=data_config_path, epochs=1, imgsz=640, batch=batch_size, lr0=learning_rate, device=device)
 
