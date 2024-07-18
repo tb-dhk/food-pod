1b8a4140d9cdb27c7c97671b82035b4ebf2d5b43
shuu-wasseo <joshuasoh2008@gmail.com>
2024-07-18 17:30:06 +0800

    .

--- bin/run.py
+++ bin/run.py
@@ -3,6 +3,7 @@ import time
 import sys
 from datetime import datetime
 import RPi.GPIO as GPIO
+from hx711 import HX711
 import cv2
 import os
 import numpy as np
@@ -20,98 +21,12 @@ GPIO.setwarnings(False)
 DT_PIN = 17
 SCK_PIN = 27
 
-def createBoolList(size=8):
-    return [False] * size
-
 def log_message(message):
     log_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
     print(f"[{log_time}] {message.lower()}")
 
-class HX711:
-    def __init__(self, dout, pd_sck, gain=128):
-        self.PD_SCK = pd_sck
-        self.DOUT = dout
-
-        GPIO.setup(self.PD_SCK, GPIO.OUT)
-        GPIO.setup(self.DOUT, GPIO.IN)
-
-        self.GAIN = 0
-        self.OFFSET = 0
-        self.SCALE = 1
-        self.lastVal = 0
-
-        self.set_gain(gain)
-
-    def is_ready(self):
-        return GPIO.input(self.DOUT) == 0
-
-    def set_gain(self, gain):
-        if gain == 128:
-            self.GAIN = 1
-        elif gain == 64:
-            self.GAIN = 3
-        elif gain == 32:
-            self.GAIN = 2
-
-        GPIO.output(self.PD_SCK, False)
-        self.read()
-
-    def read(self, timeout=10):
-        start_time = time.time()
-        while not self.is_ready():
-            if time.time() - start_time > timeout:
-                raise TimeoutError("Operation timed out after 10 seconds")
-            time.sleep(0.1)  # Small sleep to prevent busy waiting
-        dataBits = [createBoolList(), createBoolList(), createBoolList()]
-
-        for j in range(2, -1, -1):
-            for i in range(7, -1, -1):
-                GPIO.output(self.PD_SCK, True)
-                dataBits[j][i] = GPIO.input(self.DOUT)
-                GPIO.output(self.PD_SCK, False)
-
-        GPIO.output(self.PD_SCK, True)
-        GPIO.output(self.PD_SCK, False)
-
-        if all(item == True for item in dataBits[0]):
-            return self.lastVal
-
-        bits = []
-        for i in range(2, -1, -1):
-            bits += dataBits[i]
-
-        self.lastVal = int(''.join(map(str, bits)), 2)
-        return self.lastVal
-
-    def read_average(self, times=3):
-        total = 0
-        measurements = []
-        for i in range(times):
-            new = self.read()
-            measurements.append(new)
-            total += new
-        return total / times / self.SCALE
-
-    def tare(self, times=15):
-        sum = self.read_average(times)
-        log_message(f"sum set to {sum}.")
-        self.set_offset(sum)
-
-    def set_scale(self, scale):
-        self.SCALE = scale
-
-    def set_offset(self, offset):
-        self.OFFSET = offset
-
-    def power_down(self):
-        GPIO.output(self.PD_SCK, False)
-        GPIO.output(self.PD_SCK, True)
-
-    def power_up(self):
-        GPIO.output(self.PD_SCK, False)
-
 # Initialize the HX711
-hx = HX711(dout=DT_PIN, pd_sck=SCK_PIN)
+hx = HX711(dout_pin=DT_PIN, pd_sck_pin=SCK_PIN, gain_channel_A=128, select_channel='A')
 
 def clean_and_exit():
     log_message("Cleaning...")
@@ -126,7 +41,7 @@ def zero_scale():
 
 def get_weight():
     try:
-        weight = hx.read_average(5)
+        weight = hx.get_raw_data_mean(5)
         return weight
     except (KeyboardInterrupt, SystemExit):
         clean_and_exit()
@@ -164,15 +79,12 @@ def detect_food(image_path):
 def convert_results_to_area_dict(boxes):
     area_dict = {}
 
-    # Iterate over the range of boxes
     for i in range(len(boxes.xyxy)):
-        cls = int(boxes.cls[i].item())  # Extract the class ID as an integer
-        xyxy = boxes.xyxy[i].detach().cpu().numpy()  # Extract bounding box coordinates
+        cls = int(boxes.cls[i].item())
+        xyxy = boxes.xyxy[i].detach().cpu().numpy()
 
-        # Calculate area of the bounding box
         area = (xyxy[2] - xyxy[0]) * (xyxy[3] - xyxy[1])
 
-        # Accumulate the area for the corresponding class ID
         if cls in area_dict:
             area_dict[cls] += area
         else:
@@ -182,16 +94,15 @@ def convert_results_to_area_dict(boxes):
 
 def monitor_weight():
     prev_weight = get_weight()
-    time.sleep(1)  # Initial delay to allow for any transient readings
-    
+    time.sleep(1)
+
     while True:
         current_weight = get_weight()
-        weight_change = current_weight - prev_weight  # Calculate the change in weight
+        weight_change = current_weight - prev_weight
         log_message(f"Waiting for weight change (now {prev_weight}) (change {weight_change})...")
         
-        if abs(current_weight - prev_weight) > 100000:  # Adjust the threshold as needed
-            
-            pics = get_latest_pictures("images") 
+        if abs(current_weight - prev_weight) > 100000:
+            pics = get_latest_pictures("images")
             if len(pics) >= 2:
                 log_message(f"Found {len(pics)} latest pictures.")
                 image1 = cv2.imread(pics[1])
@@ -223,41 +134,35 @@ def monitor_weight():
                 results = convert_results_to_area_dict(results)
 
                 total_area = sum(results.values())
-                total_weight = weight_change  # Assume this is the weight change in grams
+                total_weight = weight_change
                 
                 raw_weights_dict = {}
                 total_raw_weight = 0
 
                 for cls in results:
-                    # Step 1: Query the Food table to find density of food given id
                     cursor.execute(f"SELECT density FROM Food WHERE id = {cls}")
                     row = cursor.fetchone()
                     if row:
                         density = float(row[0])
                         area = results[cls]
-                        height = 2  # average height of food in cm
-                        # Calculate the raw weight for this food item
+                        height = 2
                         raw_weight = area * height * density
                         total_raw_weight += raw_weight
                         raw_weights_dict[cls] = raw_weight
 
-                # Calculate the conversion factor for raw weights to match the total weight change
                 conversion_factor = total_weight / total_raw_weight if total_raw_weight else 0
 
-                # Apply the conversion factor to adjust raw weights
                 adjusted_weights_dict = {cls: weight * conversion_factor for cls, weight in raw_weights_dict.items()}
 
                 log_message(f"results: {results}")
                 log_message(f"weights: {adjusted_weights_dict}")
 
-                # Read the images as binary data
                 with open(pics[1], 'rb') as file:
                     pic_bin = file.read()
 
                 with open(pics[0], 'rb') as file:
                     pic_new = file.read()
 
-                # Insert the log into the Logs table
                 time_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
                 cursor.execute("""
@@ -269,11 +174,12 @@ def monitor_weight():
         prev_weight = current_weight
 
         time.sleep(0)
-        
-zero_scale() 
+
+zero_scale()
 log_message("Starting weight monitoring...")
 
 try:
     monitor_weight()
 except (KeyboardInterrupt, SystemExit):
     clean_and_exit()
+
