073f4c5b88a0a9343be960646507c32988e73750
shuu-wasseo <joshuasoh2008@gmail.com>
2024-07-19 09:41:10 +0800

    .

--- bin/run.py
+++ bin/run.py
@@ -18,7 +18,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 # Define the GPIO pins for the HX711
-DT_PIN = 5 
+DT_PIN = 5
 SCK_PIN = 6
 
 def log_message(message):
@@ -87,90 +87,105 @@ def convert_results_to_area_dict(boxes):
     return area_dict
 
 def monitor_weight():
+    weight_buffer = []
+    buffer_size = 10
+    anti_spike_threshold = 0.9
+    significant_change_threshold = 10000
+
     prev_weight = hx.get_weight()
+    weight_buffer.append(prev_weight)
     time.sleep(1)
 
     while True:
         current_weight = hx.get_weight()
+        weight_buffer.append(current_weight)
+        if len(weight_buffer) > buffer_size:
+            weight_buffer.pop(0)
+
         weight_change = current_weight - prev_weight
         log_message(f"Waiting for weight change (now {current_weight}) (change {weight_change})...")
-        
-        if abs(weight_change) > 10000:
-            take_picture()
-            pics = get_latest_pictures("images")
-            if len(pics) >= 2:
-                log_message(f"Found {len(pics)} latest pictures.")
-                image1 = cv2.imread(pics[1])
-                image2 = cv2.imread(pics[0])
-                
-                log_message(f"Calculating difference mask between {pics[0]} and {pics[1]}...")
-                diff_mask = find_differences(image1, image2)
-                
-                timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-                diff_filename = f"images/{timestamp}-diff.jpg"
-                cv2.imwrite(diff_filename, diff_mask)
-                
-                log_message(f"Detecting food from differences image ({diff_filename})...")
-                results = detect_food(diff_filename)
-            elif len(pics) == 1:
-                log_message("Less than two pictures available, detecting food from new picture...")
-                results = detect_food(pics[0])
-            else:
-                log_message("no images in directory. taking first picture...")
 
-            server = 'food-pod.database.windows.net'
-            database = 'food-pod'
-            username = 'foodpod'
-            password = os.getenv("SQL_PASSWORD")
-            driver = '{ODBC Driver 18 for SQL Server}'
-            cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+',1433;DATABASE='+database+';UID='+username+';PWD='+ password)
-            
-            with cnxn:
-                cursor = cnxn.cursor()
+        if len(weight_buffer) == buffer_size:
+            max_weight = max(weight_buffer)
+            min_weight = min(weight_buffer)
+            if max_weight - min_weight > significant_change_threshold and abs(weight_change) > significant_change_threshold:
+                if (current_weight > prev_weight and current_weight - min_weight > significant_change_threshold * anti_spike_threshold) or \
+                   (current_weight < prev_weight and max_weight - current_weight > significant_change_threshold * anti_spike_threshold):
+                    log_message("Significant weight change detected.")
+                    take_picture()
+                    pics = get_latest_pictures("images")
+                    if len(pics) >= 2:
+                        log_message(f"Found {len(pics)} latest pictures.")
+                        image1 = cv2.imread(pics[1])
+                        image2 = cv2.imread(pics[0])
 
-                results = convert_results_to_area_dict(results)
+                        log_message(f"Calculating difference mask between {pics[0]} and {pics[1]}...")
+                        diff_mask = find_differences(image1, image2)
 
-                total_area = sum(results.values())
-                total_weight = weight_change
-                
-                raw_weights_dict = {}
-                total_raw_weight = 0
+                        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+                        diff_filename = f"images/{timestamp}-diff.jpg"
+                        cv2.imwrite(diff_filename, diff_mask)
 
-                for cls in results:
-                    cursor.execute(f"SELECT density FROM Food WHERE id = {cls}")
-                    row = cursor.fetchone()
-                    if row:
-                        density = float(row[0])
-                        area = results[cls]
-                        height = 2
-                        raw_weight = area * height * density
-                        total_raw_weight += raw_weight
-                        raw_weights_dict[cls] = raw_weight
+                        log_message(f"Detecting food from differences image ({diff_filename})...")
+                        results = detect_food(diff_filename)
+                    elif len(pics) == 1:
+                        log_message("Less than two pictures available, detecting food from new picture...")
+                        results = detect_food(pics[0])
+                    else:
+                        log_message("No images in directory. Taking first picture...")
 
-                conversion_factor = total_weight / total_raw_weight if total_raw_weight else 0
+                    server = 'food-pod.database.windows.net'
+                    database = 'food-pod'
+                    username = 'foodpod'
+                    password = os.getenv("SQL_PASSWORD")
+                    driver = '{ODBC Driver 18 for SQL Server}'
+                    cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+',1433;DATABASE='+database+';UID='+username+';PWD='+ password)
 
-                adjusted_weights_dict = {cls: weight * conversion_factor for cls, weight in raw_weights_dict.items()}
+                    with cnxn:
+                        cursor = cnxn.cursor()
 
-                log_message(f"results: {results}")
-                log_message(f"weights: {adjusted_weights_dict}")
+                        results = convert_results_to_area_dict(results)
 
-                with open(pics[1], 'rb') as file:
-                    pic_bin = file.read()
+                        total_area = sum(results.values())
+                        total_weight = weight_change
 
-                with open(pics[0], 'rb') as file:
-                    pic_new = file.read()
+                        raw_weights_dict = {}
+                        total_raw_weight = 0
 
-                time_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                        for cls in results:
+                            cursor.execute(f"SELECT density FROM Food WHERE id = {cls}")
+                            row = cursor.fetchone()
+                            if row:
+                                density = float(row[0])
+                                area = results[cls]
+                                height = 2
+                                raw_weight = area * height * density
+                                total_raw_weight += raw_weight
+                                raw_weights_dict[cls] = raw_weight
 
-                cursor.execute("""
-                    INSERT INTO Logs (time, bin_id, picture_of_bin, filtered_picture_of_new_food, dictionary_of_estimated_amts_of_food, change_in_weight)
-                    VALUES (?, ?, ?, ?, ?, ?)
-                """, (time_now, 0, pyodbc.Binary(pic_bin), pyodbc.Binary(pic_new), str(adjusted_weights_dict), weight_change))
-                cnxn.commit()
+                        conversion_factor = total_weight / total_raw_weight if total_raw_weight else 0
+
+                        adjusted_weights_dict = {cls: weight * conversion_factor for cls, weight in raw_weights_dict.items()}
+
+                        log_message(f"results: {results}")
+                        log_message(f"weights: {adjusted_weights_dict}")
+
+                        with open(pics[1], 'rb') as file:
+                            pic_bin = file.read()
+
+                        with open(pics[0], 'rb') as file:
+                            pic_new = file.read()
+
+                        time_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+                        cursor.execute("""
+                            INSERT INTO Logs (time, bin_id, picture_of_bin, filtered_picture_of_new_food, dictionary_of_estimated_amts_of_food, change_in_weight)
+                            VALUES (?, ?, ?, ?, ?, ?)
+                        """, (time_now, 0, pyodbc.Binary(pic_bin), pyodbc.Binary(pic_new), str(adjusted_weights_dict), weight_change))
+                        cnxn.commit()
 
         prev_weight = current_weight
-
-        time.sleep(0)
+        time.sleep(1)
 
 zero_scale()
 log_message("Starting weight monitoring...")
