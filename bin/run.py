dd96704e7576489510c398ee5ce118f8dc95e233
shuu-wasseo <joshuasoh2008@gmail.com>
2024-08-06 17:35:12 +0800

    .

--- bin/run.py
+++ bin/run.py
@@ -31,13 +31,6 @@ def sql_log_message(cnxn, message):
         VALUES (?, ?, ?)
     """, (0, message.lower(), time_now))
     cnxn.commit()
-    
-    # Log to the file
-    log_message = f"{time_now} - {message.lower()}\n"
-    log_file_path = "/home/pi/startup.log"
-    
-    with open(log_file_path, 'a') as log_file:
-        log_file.write(log_message)
 
 def initialize_gpio_with_timeout(timeout=10):
     def init_gpio():
