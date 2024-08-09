945e0cb7c04db3697eb90a60d62909c0f88c878b
shuu-wasseo <joshuasoh2008@gmail.com>
2024-08-09 17:38:24 +0800

    fixed trendsview

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,21 +1,15 @@
-import { defineConfig } from 'vite';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
 
-
-// Export the Vite configuration
 export default defineConfig({
-  plugins: [
-    // Node polyfills to support Node.js built-in modules
-    nodePolyfills(),
-  ],
-  optimizeDeps: {
-    // Exclude specific packages or files from Vite's dependency optimization
-    exclude: ['@mapbox/node-pre-gyp']
-  },
-  build: {
-    rollupOptions: {
-      external: ['@mapbox/node-pre-gyp']
+  plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'localhost.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'localhost.crt')),
     }
   }
-});
+})
 
