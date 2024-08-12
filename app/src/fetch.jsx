4cc7e730bff65e881d3c9f36a868e37cfe6fe391
shuu-wasseo <joshuasoh2008@gmail.com>
2024-08-12 10:37:41 +0800

    .

--- app/src/fetch.jsx
+++ app/src/fetch.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 // Function to fetch data from an Azure SQL database
 export default async function fetchFromAzure(endpoint) {
   try {
-    const response = await axios.get(`https://localhost:5000/api/${endpoint}`);
+    const response = await axios.get(`https://food-pod.onrender.com/api/${endpoint}`);
     return response.data;
   } catch (error) {
     console.error('Error fetching data from Azure:', error);
